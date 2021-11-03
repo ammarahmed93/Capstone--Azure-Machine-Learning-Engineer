b414d6af729de45a18cde57447744b20fbb4776e
Ammar Ahmed <43079200+ammarahmed93@users.noreply.github.com>
2021-11-03 18:29:51 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -11,48 +11,39 @@ from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 from azureml.core import Workspace, Datastore, Dataset
 
-ws = Workspace.from_config()
 
-# azureml-core of version 1.0.72 or higher is required
-# azureml-dataprep[pandas] of version 1.1.34 or higher is required
 
-# create tabular dataset from a single file in datastore
-
-key = "wine_classification"
-
-if key in ws.datasets.keys(): 
-        found = True
-        dataset= ws.datasets[key] 
-
-df = dataset.to_pandas_dataframe()
+ds = Dataset.Tabular.from_delimited_files(path='https://mlstrg162678.blob.core.windows.net/azureml-blobstore-d6e2902d-56fe-40f0-8768-9ead935ab1c4/UI/11-03-2021_050710_UTC/wine-classification.csv')
 
 run = Run.get_context()
 
-print(df)
-    
-# x, y = clean_data(ds)
+x_df = ds.to_pandasframe().dropna()
 
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+x = x_df[:,:-1]
+y = x_df.pop('y')
 
-# def main():
-#     # Add arguments to script
-#     parser = argparse.ArgumentParser()
 
-#     parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-#     parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
-#     args = parser.parse_args()
+def main():
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
 
-#     run.log("Regularization Strength:", np.float(args.C))
-#     run.log("Max iterations:", np.int(args.max_iter))
+    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
 
-#     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    args = parser.parse_args()
 
-#     accuracy = model.score(x_test, y_test)
-#     run.log("Accuracy", np.float(accuracy))
+    run.log("Regularization Strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
 
-#     os.makedirs('outputs', exist_ok=True)
-#     joblib.dump(value=model, filename='outputs/model.joblib')
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+
+    accuracy = model.score(x_test, y_test)
+    run.log("Accuracy", np.float(accuracy))
+
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(value=model, filename='outputs/model.joblib')
 
 if __name__ == '__main__':
     main()
